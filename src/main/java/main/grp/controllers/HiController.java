1f6604cde7e6669af981f2dcdd8b8da36e2fe51f
KomX <komx512@outlook.com>
2025-10-25 13:13:20 +0500

    #HiController# commented

--- src/main/java/main/grp/controllers/HiController.java
+++ src/main/java/main/grp/controllers/HiController.java
@@ -1,19 +1,21 @@
-package main.grp.controllers;
+//Закомментировал, чтоб не смущало при проверке. Согласно задаче используется TaskController
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-@RestController
-@RequestMapping("/hi")
-public class HiController {
-    @GetMapping
-    public ResponseEntity<String> sayHi() {
-        return ResponseEntity.ok("HI! <br><pr>" + LocalDateTime.now());
-    }
-}
+//package main.grp.controllers;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.time.LocalDate;
+//import java.time.LocalDateTime;
+//import java.util.Date;
+//
+//@RestController
+//@RequestMapping("/hi")
+//public class HiController {
+//    @GetMapping
+//    public ResponseEntity<String> sayHi() {
+//        return ResponseEntity.ok("HI! <br><pr>" + LocalDateTime.now());
+//    }
+//}
